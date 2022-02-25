1405030164dd4184990d1229bec03fbf6f661f38
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-25 17:59:15 +0600

    navbar added

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Home;
