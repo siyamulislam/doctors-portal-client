e594e85080e0bab459ee75ff199cf40800d0b066
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-26 23:08:43 +0600

    info card modified

--- src/components/Home/Header/BusinessInfo/InfoCard/InfoCard.js
+++ src/components/Home/Header/BusinessInfo/InfoCard/InfoCard.js
@@ -2,16 +2,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const InfoCard = (props) => {
-    const{title,description,icon,backgroundColor}=props.info;
+    const { title, description, icon, backgroundColor } = props.info;
     return (
-        <div className='col-md-4'>
-            <div >
-            <FontAwesomeIcon icon={icon}/>
+        <div className='col-md-3'  style={{margin:'0px 0px'}}>
+            <div className='d-flex text-white' style={{backgroundColor:backgroundColor,padding:'30px 50px'}}>
+                <div >
+                    <FontAwesomeIcon icon={icon} />
+                </div>
+                <div> 
+                    <h6>{title}</h6>
+                    <small>{description}</small>
+                </div>
             </div>
-          <div>
-              <h6>{title}</h6>
-              <small>{description}</small>
-          </div>
         </div>
     );
 };
