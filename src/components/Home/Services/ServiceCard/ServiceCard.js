3bc6c0532332dc38853960e371f3ff3ffbda96a4
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-02-28 02:30:31 +0600

    Services done all

--- src/components/Home/Services/ServiceCard/ServiceCard.js
+++ src/components/Home/Services/ServiceCard/ServiceCard.js
@@ -9,7 +9,7 @@ const ServiceCard = ({ service }) => {
                 <img src={service.img} alt="" />
 
                 <h5 className='p-3' style={{color:'#565E69'}}>{service.title}</h5>
-                <p className='text-muted'>{service.description}</p>
+                <p className='text-secondary'>{service.description}</p>
 
             </div>
         </div>
--- src/components/Home/Services/ServiceDetails/ServiceDetails.js
+++ src/components/Home/Services/ServiceDetails/ServiceDetails.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 import treatment from '../../../../images/treatment.png'
+import './ServiceDetails.css'
 
 const ServiceDetails = () => {
     return (
         <div className='d-flex justify-content-center'>
-            <div className='row' style={{ width: '90%', margin:'100px 0px' }}>
-                <div className='col-md-4'><img src={treatment} alt="" width='400px' className='fluid' /></div>
-                <div className='col-md-8 mt-5 text-left pr-5'>
-                    <h3>Exceptional Dental
-                        Care, on Your Terms</h3>
-                    <p>It is a long established fact that a reader will be distracted
+            <div className='row ServiceDetailsContainer' >
+                <div className='col-md-5'><img src={treatment} alt="" width='440px' height='520px' className='fluid' /></div>
+                <div className='col-md-5  mt-5 text-left pr-5'>
+                    <h2>Exceptional Dental  <br />
+                        Care, on Your Terms</h2>
+                    <p className='text-secondary'>It is a long established fact that a reader will be distracted
                         by the readable content of a page when looking at its
                         layout. The point of using Lorem Ipsumis that it has
                         a more-or-less normal distribution of letters,as opposed
@@ -19,6 +20,7 @@ const ServiceDetails = () => {
                         and web page </p>
                         <button className='btn btn-primary'>Learn More</button>
                 </div>
+                <div className='offset-md-2'></div>
 
             </div>
         </div>
