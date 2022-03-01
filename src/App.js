1374f58e36b599c87cb0548311cefb4f477be56e
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-03-02 01:45:07 +0600

    appointment page added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from './components/Home/Home';
+import Appointment from './components/Appointment/Appointment';
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
           {/* <Route  path="/home"                   element={<Home/>}                                        />  
           <Route  path="/login"                  element={<Login/>}                                       />  
           <Route  path="/book/:bedType"          element={<RequireAuth><Book/></RequireAuth>}             />   */}
-          <Route  exact path="/"                 element={<Home/>}                                        />
+          <Route  exact path="/"                      element={<Home/>}                                        />
+          <Route  path="/appointment"                 element={<Appointment/>}                                        />
       </Routes>
     </Router>
   );
--- src/components/Home/Header/HeaderMain/HeaderMain.js
+++ src/components/Home/Header/HeaderMain/HeaderMain.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import chair from './../../../../images/chair.png';
 import './HeaderMain.css'
+import { useNavigate } from "react-router-dom";
 const HeaderMain = () => {
+    const navigate = useNavigate();
+    const getAppointment=()=>{
+        navigate("/appointment");
+    }
     return (
         <main style={{height:'600px'  }} className="row d-flex align-items-center header-main">
             <div className="col-md-4 offset-md-1 p-5">
                 <h1  >Your New Smile <br />Starts Here</h1>
                 <p className='text-secondary pr-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel repellat ratione illo non, atque error quae harum laboriosam iure minima!</p>
-                <button className="btn btn-primary">GET APPOINTMENT</button>
+                <button className="btn btn-primary" onClick={getAppointment}>GET APPOINTMENT</button>
             </div>
             <div className="col-md-5">
                 <img src={chair} alt="" className="img-fluid" />
