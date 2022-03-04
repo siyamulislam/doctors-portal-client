18a6e4ba7775338d96539673b018b2dd60ad5a52
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2022-03-04 23:25:32 +0600

    booking card designed

--- src/components/Home/MakeAppointment/BookAppointment/BookAppointment.js
+++ src/components/Home/MakeAppointment/BookAppointment/BookAppointment.js
@@ -1,12 +1,61 @@
-import React, { useState } from 'react';
-import "./BookAppointment.css"
+ import "./BookAppointment.css"
+import BookingCard from "./BookingCard/BookingCard";
 
-const BookAppointment = ({date}) => {
-    // const [appointDate, setAppointDate] = useState(props.appointDate); 
-    const appointDate= date.toDateString();
+const BookAppointment = ({date}) => { 
+    // const appointDate= date.toDateString();
+    const appointDateMod= date.toLocaleString('en-us',{month:'long',day:'numeric'})+','+date.getFullYear(); 
+    const bookingData = [
+        {
+            _id: '5e8df50be6e8231764dc23de',
+            id: 1,
+            subject: 'Teeth Orthodontics',
+            visitingHour: '8:00 AM - 9:00 AM',
+            totalSpace: 10
+        },
+        {
+            _id: '5e8df578e6e8231764dc23df',
+            id: 2,
+            subject: 'Cosmetic Dentistry',
+            visitingHour: '10:50 AM - 11:30 AM',
+            totalSpace: 10
+        },
+        {
+            _id: '5e8df5aee6e8231764dc23e0',
+            id: 3,
+            subject: 'Teeth Cleaning',
+            visitingHour: '5:00 PM - 6:00 PM',
+            totalSpace: 10
+        },
+        {
+            _id: '5e8df63be6e8231764dc23e1',
+            id: 4,
+            subject: 'Cavity Protection',
+            visitingHour: '7:00 AM - 8:30 AM',
+            totalSpace: 10
+        },
+        {
+            _id: '5e8df68de6e8231764dc23e2',
+            id: 5,
+            subject: 'Teeth Orthodontics',
+            visitingHour: '8:00 AM - 9:00 AM',
+            totalSpace: 10
+        },
+        {
+            _id: '5e8df6a0e6e8231764dc23e3',
+            id: 6,
+            subject: 'Teeth Orthodontics',
+            visitingHour: '8:00 AM - 9:00 AM',
+            totalSpace: 10
+        }
+    ]
     return (
-        <section className='text-center'>
-            <h1 className='text-primary'>Available Appointments on {appointDate }</h1>
+        <section className='text-center container'>
+            <h1 className='text-primary my-5'>Available Appointments on {appointDateMod }</h1>
+            <div className="row">
+                {
+                    bookingData.map(booking=> <BookingCard booking={booking} date={date} key={booking.id}></BookingCard>)
+                }
+                </div>
         </section>
     );
 };
